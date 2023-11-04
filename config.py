0c0763cfff6a1ff25e8316bc029ad49e0f9b72cd
kasym0803 <kasymbek618618@gmail.com>
2023-11-04 17:01:44 +0300

    'oqietoqf'

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-PROXY_URL = 'https://proxy.server:3128'
+PROXY_URL = 'http://proxy.server:3128'
 storage = MemoryStorage()
 TOKEN = config('TOKEN')
 bot = Bot(token=TOKEN, proxy=PROXY_URL)
